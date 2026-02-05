5274474ecfc1eeef99da84acd740c4664c0052c9
inferno0230 <mail@inferno0230.in>
2026-02-05 18:36:20 +0530

    sm8650-common: Guard AOD from tablets
    Change-Id: I88e7f672dda08336c8976ee632512f5bcb0d081d
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -303,7 +303,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
-    FrameworksResTarget \
+    FrameworksResTargetCommon \
     NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
@@ -312,6 +312,7 @@ PRODUCT_PACKAGES += \
 
 ifneq ($(TARGET_IS_TABLET),true)
 PRODUCT_PACKAGES += \
+    FrameworksResTargetPhone \
     CarrierConfigResCommon
 endif
 
